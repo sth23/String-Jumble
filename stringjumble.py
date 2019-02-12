629d79069282d91db673edd47a46f64d0984e0bb
sth23 <47009471+sth23@users.noreply.github.com>
2019-02-12 00:06:03 -0500

    Updated from Brython Server: 2/12/2019 12:06:04 AM

--- stringjumble.py
+++ stringjumble.py
@@ -1,7 +1,7 @@
 """
 stringjumble.py
-Author: <your name>
-Credit: <sources>
+Author: Sean Healey
+Credit: Tutorials, Stack Overflow
 
 Assignment:
 
@@ -25,3 +25,25 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+
+# Get user input
+original = input("Please enter a string of text (the bigger the better): ")
+print('You entered "' + original + '". Now jumble it:')
+
+# Create list of letters and list of words
+letters_list = list(original)
+words_list = original.split()
+
+# Reverse order of all characters in string
+reverse_letters = letters_list[::-1]
+print(''.join(reverse_letters))
+
+# Reverse order of words in string
+reverse_words = words_list[::-1]
+print(' '.join(reverse_words))
+
+# Reverse order of letters within words, but leave words in order
+reverse_words_letters = [len(words_list)]
+for i in range(0,len(words_list)):
+    reverse_words_letters[i] = words_list[i][::-1]
+print(' '.join(reverse_words_letters))
